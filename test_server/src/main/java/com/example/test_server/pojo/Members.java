b52e5bfd91be8ece8747209e4aae4be3a4b3cddd
Akira <62070215@kmitl.ac.th>
2021-12-13 21:42:16 +0700

    Test Deploy Proto

---
+++ test_server/src/main/java/com/example/test_server/pojo/Members.java
@@ -0,0 +1,42 @@
+package com.example.test_server.pojo;
+
+import lombok.Data;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Data
+@Document("members")
+public class Members {
+    @Id
+    private String _id;
+    private String member_user;
+    private String member_password;
+    private String member_email;
+    private String member_fname;
+    private String member_lname;
+    private String member_class;
+    private Integer member_no;
+    private String member_dob;
+    private String member_address;
+    private String member_img;
+    private String member_level;
+    private String member_timestamp;
+
+    public Members() {}
+
+    public Members(String _id, String member_user, String member_password, String member_email, String member_fname, String member_lname, String member_class, Integer member_no, String member_dob, String member_address, String member_img, String member_level, String member_timestamp) {
+        this._id = _id;
+        this.member_user = member_user;
+        this.member_password = member_password;
+        this.member_email = member_email;
+        this.member_fname = member_fname;
+        this.member_lname = member_lname;
+        this.member_class = member_class;
+        this.member_no = member_no;
+        this.member_dob = member_dob;
+        this.member_address = member_address;
+        this.member_img = member_img;
+        this.member_level = member_level;
+        this.member_timestamp = member_timestamp;
+    }
+}
